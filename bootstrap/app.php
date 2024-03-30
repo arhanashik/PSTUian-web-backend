d001e207cd347735c4dbceaefc2474bfad668ae5
maniruzzamanak <m.akash.cse@gmail.com>
2024-03-30 10:10:55 +0600

    Test API endpoint added with /api/v1/test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,13 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api/v1.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function ($router) {
+            Route::middleware('api')
+                ->prefix('api/v1')
+                ->name('api.')
+                ->group(base_path('routes/api/v1.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
