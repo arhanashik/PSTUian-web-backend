972750232cb28a2588b21f3486f82dacfa2c7941
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-03-30 20:21:47 +0600

    Setup L5-swagger

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -6,6 +6,17 @@ namespace App\Http\Controllers;
 
 use App\Traits\Responsable;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="PSTUian API Documentation",
+ *      description="API Documentation",
+ *      @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
 abstract class BaseController extends Controller
 {
     use Responsable;
