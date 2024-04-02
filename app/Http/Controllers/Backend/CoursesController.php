db38b9cfd4311fb90dcdeaccbd577b8cceb70ff6
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2024-04-02 13:11:42 +0600

    Course update api & documentation

--- app/Http/Controllers/Backend/CoursesController.php
+++ app/Http/Controllers/Backend/CoursesController.php
@@ -77,4 +77,41 @@ class CoursesController extends BaseController
             return $this->responseErrorJson($exception);
         }
     }
+
+    /**
+     * @OA\PUT(
+     *     path="/api/v1/backend/courses/{id}",
+     *     tags={"Courses"},
+     *     summary="Course update api ",
+     *     description="",
+     *     @OA\Parameter(name="id", description="id, eg; 1", required=true, in="path", @OA\Schema(type="integer")),
+     *     @OA\Parameter(name="_method", description="PUT / POST", example="PUT", required=true, in="query", @OA\Schema(type="string")),
+     *     security={{"bearer":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"course_code", "course_title", "credit_hour", "faculty_id"},
+     *             @OA\Property(property="course_code", type="string", example="CCE-311"),
+     *             @OA\Property(property="course_title", type="string", example="Computer & Communication Engineering"),
+     *             @OA\Property(property="credit_hour", type="string", example="27"),
+     *             @OA\Property(property="faculty_id", type="number", example="1"),
+     *        ),
+     *    ),
+     *    @OA\Response(response=201,description="faculty updated successfully"),
+     *    @OA\Response(response=400, description="Bad request"),
+     *    @OA\Response(response=404, description="Resource Not Found"),
+     * )
+     */
+    public function update(CourseRequest $request, int $id): JsonResponse
+    {
+        try {
+            return $this->responseJson(
+                $this->courseService->update($id, $request->all()),
+                Response::HTTP_OK,
+                __('Course updated successfully.')
+            );
+        } catch (Exception $exception) {
+            return $this->responseErrorJson($exception);
+        }
+    }
 }
