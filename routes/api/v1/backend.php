00242be06236297ebbc4c40ce3a716d406c0d7d9
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2024-04-05 19:30:18 +0600

    Session crud API

--- routes/api/v1/backend.php
+++ routes/api/v1/backend.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Backend\AccountOptionController;
 use App\Http\Controllers\Backend\CoursesController;
 use App\Http\Controllers\Backend\DonationsController;
 use App\Http\Controllers\Backend\FacultyController;
+use App\Http\Controllers\Backend\SessionController;
 use App\Http\Controllers\Backend\StudentController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,6 @@ Route::apiResource('courses', CoursesController::class)->only(['index', 'store',
 // Students
 Route::apiResource('students', StudentController::class)->only(['index', 'store', 'update', 'destroy']);
 Route::get('/students/all', [StudentController::class, 'all'])->name('students.all');
+
+// Sessions
+Route::apiResource('sessions', SessionController::class)->only(['index', 'store', 'update', 'destroy']);
