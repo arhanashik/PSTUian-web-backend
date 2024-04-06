5954b8ea94f63474fa4d9d24f833bee425d2c27a
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-04-06 15:47:23 +0600

    Employee API Added

--- routes/api/v1/backend.php
+++ routes/api/v1/backend.php
@@ -2,17 +2,19 @@
 
 use App\Http\Controllers\Backend\AcademicYearController;
 use App\Http\Controllers\Backend\AccountOptionController;
+use App\Http\Controllers\Backend\BatchController;
 use App\Http\Controllers\Backend\CoursesController;
 use App\Http\Controllers\Backend\DonationsController;
-use App\Http\Controllers\Backend\BatchController;
+use App\Http\Controllers\Backend\EmployeeController;
 use App\Http\Controllers\Backend\FacultyController;
 use App\Http\Controllers\Backend\StudentController;
 use Illuminate\Support\Facades\Route;
 
 // Donations
 Route::apiResource('donations', DonationsController::class)->only(['index', 'update', 'destroy']);
+
 // Batches
-Route::apiResource('batches', BatchController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+Route::apiResource('batches', BatchController::class);
 
 // AccountOption (Bikash, Nagod, Rocket)
 Route::apiResource('account-options', AccountOptionController::class)->only(['index', 'store', 'update', 'destroy']);
@@ -26,6 +28,9 @@ Route::apiResource('courses', CoursesController::class)->only(['index', 'store',
 // Students
 Route::apiResource('students', StudentController::class)->only(['index', 'store', 'update', 'destroy']);
 Route::get('/students/all', [StudentController::class, 'all'])->name('students.all');
+
+// Employees
+Route::apiResource('employees', EmployeeController::class);
 
 // AcademicYear
 Route::apiResource('academicyears', AcademicYearController::class)->only(['index', 'store', 'update', 'destroy']);
