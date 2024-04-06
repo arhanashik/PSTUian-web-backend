2f58cae998d1e94a1b9fc5205d8e85158e1c5ca4
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-04-06 14:25:18 +0600

    Refactor Batch service to use CrudService

--- app/Services/BatchService.php
+++ app/Services/BatchService.php
@@ -4,59 +4,17 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\Enum\DeleteStatus;
-use App\Models\Batch;
 use App\Repositories\BatchRepository;
 
-class BatchService
+class BatchService extends CrudService
 {
     const NOT_FOUND_MESSAGE = 'Batch not found.';
 
-    public function __construct(
-        private readonly BatchRepository $batchRepository
-    ) {
-    }
-
-    public function getBatches(): array
+    public function __construct(private readonly BatchRepository $batchRepository)
     {
-        return $this->batchRepository->all();
-    }
-
-    public function getPaginatedBatches(?int $perPage = null, $filters = []): array
-    {
-        $filters['deleted'] = DeleteStatus::NOT_DELETED;
-        return $this->batchRepository->paginate($perPage, $filters);
-    }
-
-    public function getBatch(int $id): Batch
-    {
-        return $this->batchRepository->find($id);
-    }
-
-    public function storeBatch(array $data): Batch
-    {
-        return $this->batchRepository->create($data);
-    }
-
-    public function updateBatch(int $id, array $data): Batch
-    {
-        $batch = $this->batchRepository->find($id);
-
-        if (!$batch) {
-            throw new NotFoundHttpException(__(self::NOT_FOUND_MESSAGE));
-        }
-
-        return $this->batchRepository->update($batch, $data);
-    }
-
-    public function deleteBatch(int $id): Batch
-    {
-        $batch = $this->batchRepository->find($id);
-
-        if (!$batch) {
-            throw new NotFoundHttpException(__(self::NOT_FOUND_MESSAGE));
-        }
-
-        return $this->batchRepository->softDelete($batch);
+        parent::__construct(
+            $batchRepository,
+            fn() => __('Batch not found.')
+        );
     }
 }
