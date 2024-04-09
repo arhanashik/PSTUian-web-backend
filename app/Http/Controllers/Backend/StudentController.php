56c3b6c4ef1643ac6509383cae3acaf541746fcf
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2024-04-09 12:11:19 +0600

    Student update api

--- app/Http/Controllers/Backend/StudentController.php
+++ app/Http/Controllers/Backend/StudentController.php
@@ -148,4 +148,56 @@ class StudentController extends BaseController
             return $this->responseErrorJson($exception);
         }
     }
+
+    /**
+     * @OA\PUT(
+     *     path="/api/v1/backend/students/{id}",
+     *     tags={"Students"},
+     *     summary="Update Student",
+     *     description="Student update api",
+     *     @OA\Parameter(name="id", description="id, eg; 1", required=true, in="path", @OA\Schema(type="integer")),
+     *     @OA\Parameter(name="_method", description="PUT / POST", example="PUT", required=true, in="query", @OA\Schema(type="string")),
+     *     security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 required={"s_id", "reg", "academicyear_id", "faculty_id", "batch_id", "email", "password", "name", "phone", "blood", "address", "image_url", "cv_link", "bio", "linkedin", "facebook"},
+     *                 @OA\Property(property="s_id", type="string", example="1802043", description="Student ID"),
+     *                 @OA\Property(property="reg", type="string", example="08453", description="Student reg no"),
+     *                 @OA\Property(property="academicyear_id", type="integer", example=1, description="Academic session id"),
+     *                 @OA\Property(property="faculty_id", type="integer", example=1, description="Faculty ID"),
+     *                 @OA\Property(property="batch_id", type="integer", example=1, description="Batch ID"),
+     *                 @OA\Property(property="email", type="string", example="shishir.cse.pstu@gmail.com", description="Student email address"),
+     *                 @OA\Property(property="password", type="string", example="password", description="Account password"),
+     *                 @OA\Property(property="name", type="string", example="John Doe", description="Name of the student"),
+     *                 @OA\Property(property="phone", type="string", example="123-456-7890", description="Phone number"),
+     *                 @OA\Property(property="blood", type="string", example="B+", description="Blood group"),
+     *                 @OA\Property(property="address", type="string", example="123 Main St, City, State, ZIP", description="Address of the student"),
+     *                 @OA\Property(property="image_url", type="string", example="https://pstuian.com/uploads/student/1802043.jpeg", description="Image URL of the student"),
+     *                 @OA\Property(property="cv_link", type="string", example="https://cv.pstuian.com/cv_1802043.pdf", description="Student CV URL"),
+     *                 @OA\Property(property="bio", type="string", example="student details", description="Student details"),
+     *                 @OA\Property(property="linkedin", type="string", example="https://www.linkedin.com/in/engg-shishir/", description="LinkedIn account URL"),
+     *                 @OA\Property(property="facebook", type="string", example="https://www.facebook.com/Engg.Shishir/", description="Facebook account URL")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response=201, description="Student updated successfully"),
+     *     @OA\Response(response=400, description="Bad request"),
+     *     @OA\Response(response=404, description="Resource Not Found")
+     * )
+     */
+    public function update(StudentRequest $request, int $id): JsonResponse
+    {
+        try {
+            return $this->responseJson(
+                $this->studentService->update($id, $request->all()),
+                Response::HTTP_OK,
+                __('Student updated successfully.')
+            );
+        } catch (Exception $exception) {
+            return $this->responseErrorJson($exception);
+        }
+    }
 }
