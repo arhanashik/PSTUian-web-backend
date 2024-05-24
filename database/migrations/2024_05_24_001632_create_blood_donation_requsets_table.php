b18dc96b91c123eb5ad3125c91b0f59c7e8febf7
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2024-05-24 06:22:21 +0600

    blood donation request model and migration

---
+++ database/migrations/2024_05_24_001632_create_blood_donation_requsets_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Enum\DeleteStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('blood_donation_requsets', function (Blueprint $table) {
+            $table->id();
+            $table->string('blood_group', 10);
+            $table->date('need_before');
+            $table->string('phone');
+            $table->text('message');
+            $table->unsignedTinyInteger('deleted')->default(DeleteStatus::NOT_DELETED);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('blood_donation_requsets');
+    }
+};
